484735b833de607722b6717fe65eb8053585de64
Nightdavisao <nightdavisao27@protonmail.com>
2026-08-01 18:30:22 -0300

    allow AM to link metadata on floating player if not cloud item

--- src/patcher.ts
+++ src/patcher.ts
@@ -1,12 +1,17 @@
+import log4js from "log4js";
+const { getLogger } = log4js;
+
+const logger = getLogger('patcher')
+
 const PATCHES = [
     // these only stops Apple Music from overriding metadata from uploaded cloud items
     {
-        name: "override 1",
-        find: /(!([a-z])\.isRadioStation\)\s*try\s*\{)\s*([a-z])\s*=\s*await\s+this\.jet\.dispatch\s*\(\s*[a-z]+\(\s*\{[\s\S]*?\}\s*\)\s*;?\)/,
-        replace: "$1$3={albumName:$2.albumName}"
+        name: "Prevent metadata linking (if the playing item is a cloud item) on floating player",
+        find: /(!([a-z])\.isRadioStation\)\s*try\s*\{)\s*([a-z])\s*=\s*await\s+(this\.jet\.dispatch\s*\(\s*[a-z]+\(\s*\{[\s\S]*?\}\s*\)\s*);?\)/,
+        replace: "$1$3=$2.isCloudItem?{albumName:$2.albumName}:$4"
     },
     {
-        name: "override 2",
+        name: "Prevent metadata linking (if the playing item is a cloud item) on immersive/LCD (?) mode",
         find: /(async fetchCatalogId\(\)\{)([^}]*\})/,
         replace: "$1if (this.currentItem && this.currentItem.isCloudItem) return;$2"
     }
@@ -18,7 +23,8 @@ export async function testPatches(script: string) {
         // reset lastIndex in case this regex has the g flag and was used before
         patch.find.lastIndex = 0
         if (patch.find.test(script)) {
-            console.log(`successfully patched with the patch ${patch.name}`)
+            // TODO: pick up the script URL!
+            logger.info(`successfully patched script code with the patch "${patch.name}"`)
             patch.find.lastIndex = 0
             // replacement argument might be a function
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
